525dfcf211aade6d95070462f1945c089d89142a
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-29 13:51:49 +0300

    code refactor

--- fifty-birds/bird5/main.lua
+++ fifty-birds/bird5/main.lua
@@ -4,8 +4,6 @@
 
     bird5
     "The Infinite Pipe Update"
-
-    Author: Colton Ogden
     cogden@cs50.harvard.edu
 
     A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
