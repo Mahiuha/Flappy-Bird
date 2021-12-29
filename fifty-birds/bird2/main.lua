0f816094fb3c321403f410b687324d967a7423ee
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-29 14:02:54 +0300

    code refactor

--- fifty-birds/bird2/main.lua
+++ fifty-birds/bird2/main.lua
@@ -1,6 +1,4 @@
 --[[
-    GD50
-    Flappy Bird Remake
     A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
     but effective gameplay mechanic of avoiding pipes indefinitely by just tapping 
     the screen, making the player's bird avatar flap its wings and move upwards slightly. 
