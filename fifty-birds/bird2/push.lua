78040083e80cbb78a1cb53b5a56b8e18d9965a73
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-29 14:04:04 +0300

    code refactor

--- fifty-birds/bird2/push.lua
+++ fifty-birds/bird2/push.lua
@@ -1,6 +1,3 @@
--- push.lua v0.4
-
-
 local love11 = love.getVersion() == 11
 local getDPI = love11 and love.window.getDPIScale or love.window.getPixelScale
 local windowUpdateMode = love11 and love.window.updateMode or function(width, height, settings)
