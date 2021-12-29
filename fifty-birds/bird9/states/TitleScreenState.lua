657db3d63e68d31524c1c201a22241bdd8f6495a
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-29 13:35:17 +0300

    code refactor

--- fifty-birds/bird9/states/TitleScreenState.lua
+++ fifty-birds/bird9/states/TitleScreenState.lua
@@ -1,8 +1,5 @@
 --[[
     TitleScreenState Class
-    
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
 
     The TitleScreenState is the starting screen of the game, shown on startup. It should
     display "Press Enter" and also our highest score.
