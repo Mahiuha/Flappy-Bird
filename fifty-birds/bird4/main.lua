81b9d9638b766db94e6a9538969426bf74e52169
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-29 13:54:40 +0300

    code refactor

--- fifty-birds/bird4/main.lua
+++ fifty-birds/bird4/main.lua
@@ -4,7 +4,6 @@
 
     bird4
     "The Anti-Gravity Update"
-    cogden@cs50.harvard.edu
 
     A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
     but effective gameplay mechanic of avoiding pipes indefinitely by just tapping 
