eff843fbd19756c0e151b4ff66a43731fa61b644
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-31 01:23:37 +0300

    refactor

--- fifty-birds/bird0/push.lua
+++ fifty-birds/bird0/push.lua
@@ -2,7 +2,6 @@
 
 -- Copyright (c) 2020 Ulysse Ramage
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
--- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 local love11 = love.getVersion() == 11
 local getDPI = love11 and love.window.getDPIScale or love.window.getPixelScale
